f9c81b26bb0cb6a9a270d79d975c61ea6858ed4e
Zac Wolff <zacharyhussein@gmail.com>
2015-09-11 08:02:58 +0000

    Initial Commit

---
+++ Swifty JSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swifty JSON
+//
+//  Created by Zac Wolff on 9/11/15.
+//  Copyright (c) 2015 GH4HUB Ghana Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
